fc27a6ca7ddb13647216646cf4f1b27f8098547e
David Termux <davidarango82@gmail.com>
2024-03-09 15:10:00 +0100

    urls and patient views not working

--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Patient
 
-# Create your views here.
+class IndexView (generic.ListView): 
+    template_name = "patients/index.html"
+#    context_object_name = "patient_list" 
+        
+#    def get_queryset ( self ): 
+#        """Return xxx""" 
+#        return Patient.objects.order_by ("lastname")[:5]
