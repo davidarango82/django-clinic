027e27e310ab93448c0dae741da55885e6adc73f
David Lenovo T440p <davidarango82@gmail.com>
2024-04-16 23:02:49 +0200

    updated timezone, patients.models, patients.templates

--- patients/models.py
+++ patients/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Patient(models.Model):
-    medical_record = models.CharField(max_length=20)
     name = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
-    DNI = models.CharField(max_length=20)
-    birth_date = models.DateField("Birth Date (YYYY-MM-DD)")
+    NIF = models.CharField(max_length=20, unique=True)
+    birth_date = models.DateField(help_text="(YYYY-MM-DD)")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    #medical_record = models.CharField(max_length=20) #possibly for future use. Relationship w/ CRM/ERP
--- patients/views.py
+++ patients/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.views import generic
 from .models import Patient
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
-class IndexView (generic.ListView): 
+class IndexView (ListView): 
     template_name = "patients/index.html"
     context_object_name = "latest_patient_list" 
         
@@ -13,7 +13,7 @@ class IndexView (generic.ListView):
         """Return xxx""" 
         return Patient.objects.order_by ("lastname")[:5]
 
-class PatientCreate(LoginRequiredMixin, CreateView):
+class PatientCreate(CreateView):
     model = Patient
     fields = '__all__'
     success_url = reverse_lazy('patients:all')
